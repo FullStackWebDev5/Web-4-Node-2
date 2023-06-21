5bf1f66fcd34e74630f31b830ae0658eb564f926
Ankit Kumar Mishra <ankit@techpassport.io>
2023-06-21 23:33:07 +0530

    Node Express Server

---
+++ index.js
@@ -0,0 +1,118 @@
+const express = require('express');
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Everything is working fine!')
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/login.html')
+})
+
+app.get('/products', (req, res) => {
+    const products = [
+        {
+            id: 1,
+            title: 'Boat Headphones',
+            price: '$78.2',
+        }, {
+            id: 2,
+            title: 'JBL Headphones',
+            price: '$92.5',
+        }
+    ]
+    res.json(products)
+})
+
+app.get('/students', (req, res) => {
+    const students = [
+        {
+            id: 1001,
+            name: 'Prabal',
+            batchNo: 4,
+        }, {
+            id: 1002,
+            name: 'Manish',
+            batchNo: 8,
+        },
+        {
+            id: 1003,
+            name: 'Chandan',
+            batchNo: 12,
+        }
+    ]
+    res.send(students)
+})
+
+app.get('/download-nodejs-logo', (req, res) => {
+    res.download(__dirname + '/nodejs-logo.png')
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000')
+})
+
+
+
+
+
+/*
+    HTTP Methods:
+    - GET: 'Read' data
+    - POST: 'Create' data
+    - PUT/PATCH: 'Update' data
+    - DELETE: 'Delete' data
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    const http = require('http');
+
+    const server = http.createServer((req, res) => {
+        res.end('We have learnt basic Node server!');
+    });
+
+    server.listen(3000, () => {
+        console.log(`Server running at http://localhost:3000`);
+    });
+*/
